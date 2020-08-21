cd936691c08a337bdb3d41ea0ee3c949366cd37c
Prafull Goswami <prafullgoswami@gmail.com>
2020-08-21 16:46:51 +0530

    Async_Controller

--- MVC_Practice/Async_Controller/Controllers/HomeController.cs
+++ MVC_Practice/Async_Controller/Controllers/HomeController.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Async_Controller.Models;
+using System.Threading.Tasks;
 
 namespace Async_Controller.Controllers
 {
     public class HomeController : Controller
     {
+        MVC_PracticeEntities me = new MVC_PracticeEntities();
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public  async  Task<ActionResult> Index(tbl_Async model)
+        {
+            if (ModelState.IsValid)
+            {
+                me.tbl_Async.Add(model);
+                await me.SaveChangesAsync();
+
+                RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+
+
 
         public ActionResult About()
         {
@@ -26,5 +46,7 @@ namespace Async_Controller.Controllers
 
             return View();
         }
+
+
     }
 }
