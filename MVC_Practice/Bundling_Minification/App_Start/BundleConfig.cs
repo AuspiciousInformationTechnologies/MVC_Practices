6738af43d080e2ba0da2204f33d6018f27aa2523
Prafull Goswami <prafullgoswami@gmail.com>
2020-08-22 12:21:56 +0530

    Bundeling_Minification

---
+++ MVC_Practice/Bundling_Minification/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Bundling_Minification.App_Start
+{
+    public class BundleConfig
+    {
+
+        public static void RegisterBundle(BundleCollection bndlescollection)
+        {
+            var bundle = new ScriptBundle("~/bundles/JS");
+
+            bundle.Include(
+                            "~/Content/JS/JavaScript1.js",
+                            "~/Content/JS/JavaScript2.js",
+                            "~/Content/JS/JavaScript3.js"
+                );
+
+            bndlescollection.Add(bundle);
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
