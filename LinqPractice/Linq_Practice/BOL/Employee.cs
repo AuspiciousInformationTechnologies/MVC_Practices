6036951900e93f8ffa7cf2fba050d28df4e9e413
Prafull Goswami <prafullgoswami@gmail.com>
2020-08-24 21:56:40 +0530

    Linq Practice Part1

---
+++ LinqPractice/Linq_Practice/BOL/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOL
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string ContactNo { get; set; }
+        public string EmailID { get; set; }
+    }
+}
--- LinqPractice/Linq_Practice/Linq_Practice/Program.cs
+++ LinqPractice/Linq_Practice/Linq_Practice/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Linq_Practice;
+using BOL;
 
 namespace Linq_Practice
 {
@@ -10,6 +12,41 @@ namespace Linq_Practice
     {
         static void Main(string[] args)
         {
+            List<Employee> employee = new List<Employee>()
+            {
+                new Employee {EmployeeName="Prafull Kumar Goswami",ContactNo="9616157858",EmailID="prafullgoswami@gmail.com",ID=1 }
+
+            };
+
+            var basicQuery = (from emp in employee
+                           select emp).ToList();
+
+            foreach(var item in basicQuery)
+            {
+
+                Console.WriteLine($"ID={item.ID},\n Name={item.EmployeeName},\n Email ID={item.EmailID},\n Contact No={item.ContactNo}");
+                Console.ReadLine();
+            }
+
+
+            var basicQuery1 = (from emp1 in employee
+                              select new
+                              {
+                                  emp1.EmployeeName,
+                                  emp1.ID,
+                                  emp1.ContactNo,
+                                  emp1.EmailID,
+                              }).ToList();
+
+
+
+            foreach (var item in basicQuery1)
+            {
+
+                Console.WriteLine($"ID={item.ID},\n Name={item.EmployeeName},\n Email ID={item.EmailID},\n Contact No={item.ContactNo}");
+                Console.ReadLine();
+            }
+
         }
     }
 }
